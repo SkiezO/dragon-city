c6a4f585c4b234bc586b2325ab34e80984a00329
Fabrizio Marmitt <fabriziomarmitt@gmail.com>
2017-05-21 13:55:58 +0200

    improvements multithread

--- app/main/src/DataLoader/File.py
+++ app/main/src/DataLoader/File.py
@@ -7,30 +7,21 @@ from app.main.src.DataLoader.ChunkProcessors import ChunkProcessor
 
 class File:
     @staticmethod
-    def innerLambda(cp, jr):
+    def worker(cp, jr):
         cp.process_chunk(jr['lines'])
         return jr
-
 
     @staticmethod
     def read(file: str, chunk_processor: ChunkProcessor, progress_reporter: ProgressReporter):
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
         jobs = []
         for chunkStart, chunkSize in ChunkProcessor.chunkify(file):
-            jobs.append(pool.apply_async(ChunkProcessor.read_lines_in_chunk, (file, chunkStart, chunkSize)))
+            chunk_result = ChunkProcessor.read_lines_in_chunk(file, chunkStart, chunkSize)
+            jobs.append(pool.apply_async(File.worker, (chunk_processor, chunk_result)))
             progress_reporter.increase_file_size(chunkSize)
 
         progress_reporter.reset_clock()
-        jobs2 = []
         for job in jobs:
-            job_result = job.get()
-            #chunk_processor.process_chunk(job_result['lines'])
-            #progress_reporter.process_chunk(job_result['chunk_size'], job_result['lines'])
-            jobs2.append(pool.apply_async(File.innerLambda, (chunk_processor, job_result)))
-            jobs2.append(pool.apply_async(File.innerLambda, (chunk_processor, progress_reporter, job_result)))
-
-
-        for job in jobs2:
             job_result = job.get()
             progress_reporter.process_chunk(job_result['chunk_size'], job_result['lines'])
 
